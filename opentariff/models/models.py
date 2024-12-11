3ece0bbe40c247ce5c9d4db7a1baa47ab36aa774
Clem <clem@futureenergy.associates>
2024-12-11 13:07:42 +0000

    model updates

--- opentariff/models/models.py
+++ opentariff/models/models.py
@@ -27,10 +27,16 @@ class RateType(str, enum.Enum):
     demand_tiered = "demand_tiered"
 
 
+class TCRBand(str, enum.Enum):
+    BAND_1 = "1"
+    BAND_2 = "2"
+    BAND_3 = "3"
+    BAND_4 = "4"
+
+
 class TariffType(str, enum.Enum):
     fixed = "fixed"
     variable = "variable"
-
 
 class ExitFeeType(str, enum.Enum):
     fixed = "fixed"
@@ -41,13 +47,35 @@ class PaymentMethod(str, enum.Enum):
     prepayment = "prepayment"
     cash_cheque = "cash_cheque"
 
+class OtherProductsType(str, enum.Enum):
+    utility = "utility"
+    physical_asset = "physical_asset"
+
+class OtherProducts(BaseModel):
+    type: OtherProductsType
+    name: str
+    description: str | None = None
+
+
+class ProductAttributes(BaseModel):
+    smart: bool | None = None
+    ev: bool | None = None
+    exclusive: bool | None = None
+    retention: bool | None = None
+    acquisition: bool | None = None
+    collective_switch: bool | None = None
+    green_percentage: float | None = None
+    bundled_products: list[OtherProducts] | None = None
+
 
 class Product(BaseModel):
     name: str
+    domestic: bool
     description: str | None = None
     type: TariffType | None = None
     available_from: datetime.datetime | None = None
     available_to: datetime.datetime | None = None
+    attributes: ProductAttributes | None = None
 
 
 class Tariff(BaseModel):
@@ -55,34 +83,31 @@ class Tariff(BaseModel):
     rate_type: RateType 
     fuel_type: Fuel 
     payment_method: PaymentMethod
+    tcr_band: TCRBand | None = None
+    standing_charge: float
     contract_length_months: int | None = None
     end_date: datetime.date | None = None
     exit_fee_type: ExitFeeType | None = None
     exit_fee_value: float | None = None
 
-class SingleRate(BaseModel):
-    unit_rate: float
-    standing_charge: float
+class RateBase(BaseModel):
+    unit_rate: float = Field(..., gt=0, lt=100)
     valid_from: datetime.datetime | None = None
     valid_to: datetime.datetime | None = None
 
-class TimeofUseRateStatic(BaseModel):
+
+class SingleRate(RateBase):
+    pass
+
+
+class TimeofUseRateStatic(RateBase):
     time_from: datetime.time
     time_to: datetime.time
     day_of_week: DayOfWeek = DayOfWeek.ALL
     month_from: int = Field(..., ge=1, le=12)
     month_to: int = Field(..., ge=1, le=12)
 
-    unit_rate: float
-    standing_charge: float
-
-    
-    valid_from: datetime.datetime | None = None
-    valid_to: datetime.datetime | None = None
-
-class TimeOfUseRateDynamic(BaseModel):
-    time: datetime.time
-    unit_rate: float
-    standing_charge: float
+class TimeOfUseRateDynamic(RateBase):
+    datetime: datetime.datetime
     dst: bool | None = Field(default=None)
 
