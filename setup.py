5eef9f16e3356c1bb426105848309abdc89debb9
Clem <clem@futureenergy.associates>
2024-12-11 09:35:10 +0000

    repo name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf8", errors="ignore") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="OpenTariff",
+    name="open-tariff",
     version="0.0.1",
     author="Future Energy Associates",
     author_email="hello@futureenergy.associates",
